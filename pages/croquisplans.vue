5690b2ad493c04d25fcb62fabf31d8256f6dcc04
Sebastien Manolios <sebastien.manolios@gmail.com>
2021-03-20 08:34:50 +0100

    Testing 2

--- pages/croquisplans.vue
+++ pages/croquisplans.vue
@@ -1,202 +1,54 @@
 <template>
-  <div>
-          <font-awesome-icon icon="dollar-sign"  style="font-size: 30px"/>
-                <font-awesome-layers class="fa-4x">
-                  <font-awesome-icon icon="circle"/>
-        <font-awesome-icon icon="check" transform="shrink-6" :style="{ color: 'white' }"/>
-      </font-awesome-layers>
-      <font-awesome-layers full-width class="fa-4x">
-        <font-awesome-icon icon="calendar"/>
-        <font-awesome-layers-text transform="shrink-8 down-3" value="27" class="fa-inverse" />
-      </font-awesome-layers>
-
-
-<h1> Tiny slider example</h1>
-<div class="slider-container">
-    <ul class="controls" id="customize-controls" aria-label="Carousel Navigation" tabindex="0">
-        <li class="prev" data-controls="prev" aria-controls="customize" tabindex="-1">
-<font-awesome-icon :icon="['fab', 'linkedin']" />        </li>
-        <li class="next" data-controls="next" aria-controls="customize" tabindex="-1">
-            <i class="fas fa-angle-right fa-5x"></i>          
-        </li>
-    </ul>
-    <div class="my-slider">
-        <div class="slider-item">
-            <div class="card">                
-                <img src="https://placeimg.com/200/150/any" alt="">
-                <h2>Title 1</h2>
-                <p class="card_description">Loresm ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas!</p>
-            </div>
-        </div>
-        
-        <div class="slider-item">
-            <div class="card">                
-                <img src="https://placeimg.com/200/150/nature" alt="">                       
-                <h2>Title 2</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas!</p>
-            </div>
-        </div>
-        
-        <div class="slider-item">
-            <div class="card">                
-                <img src="https://placeimg.com/200/150/nature/2" alt="">
-                <h2>Title 3</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas!</p>
-            </div>
-        </div>
-        <div class="slider-item">
-            <div class="card">                
-                <img src="https://placeimg.com/200/150/nature/3" alt="">
-                <h2>Title 4</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, voluptas!</p>
-            </div>
-        </div>
-    </div>    
-</div>
-
-
-
-  </div>
+    <div>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nobis, ipsum quod ab deserunt quam hic incidunt similique quia, temporibus magni, molestias animi rem veniam assumenda earum quas! Rem, alias earum.
+        Voluptas dolorum quam quas in modi eum suscipit ducimus voluptate expedita, soluta officiis dolores libero, laudantium repudiandae. Voluptas in quas, harum corrupti fugiat molestiae dolore, facilis nulla odit, possimus sunt.
+        Quis nobis dicta unde earum repellendus eligendi error molestias, id rerum eos labore iusto maiores, voluptas sapiente illo dolor exercitationem quidem mollitia blanditiis. Magnam blanditiis sint veritatis deserunt tenetur assumenda!
+        Ipsa delectus iure itaque aperiam quo, suscipit beatae perferendis natus est dignissimos doloremque quos consequatur illo nulla temporibus, accusamus laudantium excepturi accusantium magni ut nisi, ex maxime impedit! Illum, officia.
+        Praesentium error quis obcaecati dolore perspiciatis ex adipisci, dignissimos, veritatis porro, laboriosam maxime id cumque aliquid tempore sapiente libero commodi enim iste ipsam. Id deserunt voluptate ullam magni qui aliquam.
+        Ullam animi qui, amet dignissimos aut adipisci atque accusamus consectetur pariatur voluptate illum maiores laboriosam rerum, ut, voluptatibus placeat architecto nulla provident sapiente. Quas, nobis repudiandae consequatur magni ipsa aperiam.
+        Recusandae assumenda eius officia laboriosam, sed voluptatum. A, explicabo nisi! Veritatis id dolore cumque delectus at ratione quod. Velit perferendis fugiat quos voluptatem beatae molestiae? Placeat in blanditiis alias eaque?
+        Repellat ratione sunt placeat dicta itaque, ea perferendis hic quidem nihil, facilis pariatur consequatur, libero asperiores eaque! Similique beatae hic qui ex excepturi suscipit sit eum nesciunt sunt. Et, atque!
+        Hic nemo deserunt nulla distinctio laboriosam sit quae, rem perferendis quas sunt. Repellat sit facere perspiciatis aliquid sed, porro suscipit quam ea libero voluptatibus neque obcaecati quo fuga deleniti accusantium.
+        Quae sapiente ipsa illo deleniti a. Quidem magni cumque labore nam, voluptas hic. Esse consectetur nemo libero eum in harum, sunt vel ut eligendi? Aliquid rerum quae est nemo deserunt.
+        Animi, deserunt ratione enim necessitatibus, molestias cum sed quasi aliquid et quidem quas sunt autem beatae distinctio porro fugiat voluptas harum ducimus culpa quod expedita laboriosam alias natus! Eum, maiores.
+        Laudantium sint tempore culpa consequuntur ea a in deserunt neque modi commodi nam omnis ratione veniam asperiores, harum quasi doloribus atque temporibus fuga ipsam illo inventore! Ratione quam nostrum exercitationem!
+        Amet ea enim at? Dolorum qui, iste minima fugiat consequatur quas voluptates saepe, mollitia nobis excepturi doloribus perspiciatis maiores, corporis asperiores fuga unde voluptate consequuntur magni quos amet! Non, suscipit.
+        Atque quisquam delectus blanditiis, corrupti obcaecati error, reiciendis voluptas, deleniti ex omnis sint aut iste! Omnis praesentium odio blanditiis, sed, in id laudantium, laboriosam porro optio reprehenderit facere esse tenetur.
+        Ratione fugiat id itaque totam veniam quibusdam explicabo numquam. Necessitatibus eos natus iste exercitationem facere eligendi, obcaecati, accusamus, nostrum maiores inventore quasi sunt dolor. Dolor hic illo iure perferendis rem.
+        Cum soluta quaerat, ad reiciendis impedit fugit libero recusandae quo asperiores blanditiis hic. Sint dignissimos expedita minima aspernatur modi placeat dolor autem, tempora inventore aperiam, accusantium maiores voluptas beatae ducimus.
+        Eos aut animi eligendi magni voluptates corrupti repellat facere assumenda aspernatur velit ea est sint, tenetur magnam doloribus, corporis fugiat error nemo! Quidem voluptates nostrum deserunt incidunt, sit quam itaque!
+        Aliquid, nam velit ut repudiandae corporis recusandae ea id totam esse cupiditate ratione unde ipsum corrupti error rem voluptas deserunt nemo similique alias perspiciatis! Inventore quia velit ex nisi aperiam!
+        Nisi corporis libero nulla laboriosam blanditiis asperiores accusamus. Ex totam aspernatur quaerat veritatis odio, esse dolore quis? Quis blanditiis suscipit maiores expedita, iste impedit, culpa, dolores natus eum rem illo.
+        Rerum, delectus tempora maiores hic eos quasi quibusdam laboriosam quidem magnam fugiat dolores velit, officiis, rem debitis nihil! Voluptates nihil ex quibusdam distinctio sequi. Nemo voluptate saepe ipsa nam nulla.
+        Quas iste enim quisquam voluptatum quos sed assumenda quae aperiam, quibusdam veniam? Eius quae rerum, voluptatum optio ullam cupiditate necessitatibus ex placeat aliquid iste similique qui! Cumque in aut quidem.
+        Recusandae ipsum ipsam nostrum ad aspernatur veritatis consequatur eveniet, aut placeat ratione quia cum! Autem nesciunt voluptate quia totam! Culpa suscipit tempora voluptas perspiciatis asperiores sint odit quo libero esse?
+        Nostrum inventore fugiat et ipsa eos quae ex? Suscipit illo sed debitis non inventore aliquam velit harum nobis, dolore minima molestiae blanditiis facere, ex neque voluptas amet quibusdam id! Id!
+        Saepe, eos quia optio architecto incidunt totam minus delectus natus vel inventore officiis. Asperiores deleniti magnam, soluta pariatur impedit modi magni ut odio maxime excepturi beatae obcaecati cumque nihil nulla.
+        Perferendis, doloremque, tempora similique nostrum aperiam dolores maiores corrupti fugit repudiandae esse ad ullam praesentium vel deleniti dolorum earum, aliquam architecto quod nihil quae? Voluptatibus, deserunt! Repudiandae perspiciatis exercitationem assumenda!
+        Recusandae illum vitae, distinctio laudantium deserunt dicta eveniet tempora facilis mollitia veniam atque sequi quas a aut eius blanditiis magnam iure ratione sint corporis! Earum doloribus deleniti voluptas iure fugit.
+        Blanditiis magni mollitia placeat consequuntur, sit vitae voluptatibus officiis praesentium illum voluptatum sed rerum, nihil maiores doloremque aperiam impedit omnis neque dolorum quo. Enim praesentium maiores aperiam voluptatibus alias magni.
+        Minus voluptatem sit quia molestias aliquam itaque nulla eos. Odio molestias harum quis ut sapiente aperiam non labore quia qui, quae eum eveniet ab distinctio suscipit est eos! Molestiae, ipsum.
+        Quisquam, tempora dignissimos totam ut quidem corporis molestiae a adipisci, voluptates repudiandae ducimus voluptate tempore! Maiores blanditiis est dolorem tempora provident eaque accusantium optio sint laudantium temporibus, reprehenderit ea voluptatibus!
+        Mollitia ipsum, natus aperiam ipsam ipsa, voluptates officiis velit iure voluptatum omnis similique recusandae quam veritatis asperiores laboriosam nisi ut adipisci esse animi odio quod? Aspernatur quos nulla laborum perspiciatis.
+        Esse sequi, doloribus explicabo eligendi animi doloremque deserunt sunt commodi tempore, neque deleniti aut illo aspernatur maiores similique voluptatum earum veritatis labore. Temporibus quaerat dicta enim provident aliquam cupiditate debitis.
+        Ex nesciunt laudantium, temporibus vitae porro maxime, obcaecati saepe illo voluptatibus quae corrupti pariatur suscipit. Nisi expedita ab praesentium itaque corrupti aliquam iusto voluptates assumenda, fugiat iste officia voluptatem deserunt!
+        Ab laborum cumque ea nulla quam repudiandae nemo quia explicabo? Provident modi non, sapiente eligendi eum tempora deleniti, culpa aspernatur alias magnam exercitationem, eveniet iure voluptatem quasi perspiciatis saepe nihil?
+        Vitae, debitis. Eligendi consectetur ipsam sed neque veniam excepturi vero dolorem adipisci quia maiores nesciunt debitis alias rerum voluptatibus praesentium voluptates dignissimos quasi qui repellendus soluta aspernatur maxime, temporibus quas.
+        Voluptas, officiis ipsa amet recusandae consequatur ea voluptatum suscipit, tenetur doloremque accusantium autem atque aliquam nisi nihil laborum blanditiis ullam. Cupiditate, explicabo? Exercitationem amet magnam libero aut molestias voluptatem eos?
+        Doloribus eveniet ad, amet voluptates minus aspernatur, ipsum voluptatem non, aliquam asperiores ipsam nostrum assumenda. Ad sint mollitia, ipsum similique excepturi iure, aspernatur totam minima corporis, dignissimos odio est officia?
+        Fuga deserunt est distinctio odio expedita sint! Quas dolor, sunt velit doloribus quaerat soluta nam blanditiis qui tempore. Quas voluptatem possimus provident. Illum saepe reiciendis quibusdam beatae odio in dolor.
+        Repellendus molestiae delectus pariatur perspiciatis corrupti maiores, laborum animi asperiores dolor non minima officiis ex! Nobis iure, cum quis dolore omnis voluptatem amet corrupti culpa maiores incidunt magnam, eaque explicabo?
+        Dolores, asperiores repellat iste ab numquam suscipit eveniet. Autem quia facilis dignissimos voluptas tempore porro est exercitationem a perferendis. Suscipit qui numquam praesentium inventore in possimus iure amet fuga excepturi!
+        Ex quas rem delectus cupiditate labore voluptate veniam corporis, in maiores. Quia, voluptatibus quibusdam! Doloribus molestiae modi obcaecati maiores officiis eaque, distinctio tempora, quis neque quae praesentium cum, exercitationem ullam!</p>
+    </div>
 </template>
 
 <script>
-  export default {
-    head() {
-      return {
-        link: [
-          {
-            rel: "stylesheet",
-            href:
-              "https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.1/tiny-slider.css"
-          }
-        ]
-        };
-    },
-    data() {
-      return {
-        container: '.my-slider',
-    loop: true,
-    items: 1,
-    slideBy: 'page',
-    nav: false,    
-    autoplay: true,
-    speed: 400,
-    autoplayButtonOutput: false,
-    mouseDrag: true,
-    lazyload: true,
-    controlsContainer: "#customize-controls",
-    responsive: {
-        640: {            items: 2,        },
-        768: {            items: 3,        }
+    export default {
+        
     }
-
-    }
-  }
-  }
 </script>
 
 <style lang="scss" scoped>
-
-body {
-   background: #333;
-    color: white;
-}
-.slider-container {    
-    padding: 2rem 3rem;
-    background: #e6e6e6;
-    margin: 2rem auto 0 auto;
-    overflow: hidden;
-    border-radius: 5px;    
-    position:relative;
-    &:before {
-        content: '.slider-container';
-        position: absolute;
-        left: 0;
-        top: 0;
-        color: blue;
-    }
-}
-
-img {
-    display: inline-block;
-    vertical-align: top;
-    max-width: 100%;
-}
-
-.my-slider {
-    display: flex;
-    position: relative;    
-}
-
-.slider-item {
-    background: grey;
-    border-right: 2px solid #333;
-    padding: 1.5rem .75rem;
-    position: relative;
-    &:before {
-        content: '.slider-item';
-        position: absolute;
-        left: 0;
-        top: 0;
-        color: blue;
-    }
-    
-    &:hover {
-        cursor: pointer;
-    }
-}
-
-.controls {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    position: absolute;
-    left: 0;    
-    background: #333;
-    width: 100%;    
-    top: 50%;
-    transform: translatey(-50%);
-    display: flex;
-    align-items: center;    
-    justify-content: space-between;
-    &:focus {
-        outline: none;
-    }
-
-    li:hover {
-        cursor: pointer;        
-    }
-}
-
-.card {
-    text-align: center;    
-    margin: 0 auto;
-    max-width: 200px;       
-    color: #333;
-    background: white;
-    border-radius: 5px;
-    padding-bottom: .75rem;
-    font-family: sans-serif;
-    overflow: hidden;
-    box-shadow: 2px 2px rgba(black, .3);
-    position: relative;
-    &:before {
-        content: '.card';
-        position: absolute;
-        bottom: 0;
-        color: blue;
-    }
-    img {
-        margin-bottom: .25rem;
-    }
-    
-    h2 {
-        margin: 0 0 .25rem;
-    }
-    p {
-        margin: 0;
-        padding: 0 .5rem;
-    }
-}
 
 </style>
